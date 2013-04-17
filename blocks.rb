1562e24440e4acb258ebfe7debfc67194f7f725c
Katie Arbuckle <katiejo72@gmail.com>
2013-04-16 20:53:10 -0400

    added silly stuff for learning

--- blocks.rb
+++ blocks.rb
@@ -1,2 +1,11 @@
 array = :green, :purple, :pink, :orange, :turquoise, :emerald
 array.each {|color| puts color}
+
+def neweach(array)
+	lengthvar = array.length
+	lengthvar.times do |index|
+		yield array[index]
+	end
+end
+
+neweach(array) {|color| puts color}
