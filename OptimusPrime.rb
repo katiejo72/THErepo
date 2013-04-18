423852f2e912510c102851a32c1e046977f3b724
Katie Arbuckle <katiejo72@gmail.com>
2013-04-18 17:33:50 -0400

    Project Euler problem 3, made a pretty "flower box"

---
+++ OptimusPrime.rb
@@ -0,0 +1,15 @@
+#******************************************************
+#
+#  OptimusPrime.rb
+#
+#  This is a pretty "flower box" for this 
+#  spiffy little Ruby program.
+#
+#  This program is going to find the largest
+#  prime factor of a very large number.
+#
+#  This is problem 3 on Project Euler.  (Apparently 
+#  pronounced like Oiler even though it's spelled like 
+#  Yuler.)
+#
+#******************************************************
