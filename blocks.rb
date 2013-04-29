8320afee0dec98e72c00ada00c64b3f2bff53495
Katie Arbuckle <katiejo72@gmail.com>
2013-04-28 20:50:32 -0400

    added notes

--- blocks.rb
+++ blocks.rb
@@ -1,7 +1,7 @@
 array = :green, :purple, :pink, :orange, :turquoise, :emerald
-array.each {|color| puts color}
+array.each {|color| puts color}   #returns each value stored in the array
 
-def neweach(array)
+def neweach(array)                      #lines 4-11 do the exact same thing as line 2
 	lengthvar = array.length
 	lengthvar.times do |index|
 		yield array[index]
@@ -16,12 +16,12 @@ num = 5, 56, 34, 17, 22, 39, 44, 101, 77
 #map, select, reduce
 
 #map
-puts num.map {|digit| digit * 2}
+puts num.map {|digit| digit * 2}  #returns the product of each number in the array * 2
 
 #reduce
 
-puts num.reduce(5) {|sum,digit| sum + digit}
+puts num.reduce(5) {|sum,digit| sum + digit}  #returns the sum off all numbers in the array (395) + 5 = 400
 
 #select
 
-puts num.select {|digit| digit.odd?}
+puts num.select {|digit| digit.odd?}  #returns all the odd numbers in the array
