868d47a9269b2d59380c493969fc573cc5b9880f
Katie Arbuckle <katiejo72@gmail.com>
2013-04-28 21:30:09 -0400

    Working code. Woohoo!

--- OptimusPrime.rb
+++ OptimusPrime.rb
@@ -14,9 +14,58 @@
 #
 #******************************************************
 
-def findFactor(n)
-	#some mathy code goes here
+require "prime"
+
+class Primes
+
+	attr_accessor :n
+
+	def initialize(n)
+		@n = n
+	end
+
+	def findFactors
+		#find all the prime numbers up to the square root of n and store in an array, primes
+		x = 2
+		j = Math.sqrt(@n)
+		primes = Array.new
 	
+		while x <= j
+
+			if x.prime?
+				primes.push(x)
+			end
+
+			x += 1
+
+		end
+
+		#find all the numbers stored in the array, primes, that are factors of n
+		i = 0
+		v = 0
+		factors = Array.new
+
+		while i < primes.length
+
+			v = primes[i]
+			
+			if @n % v == 0
+				factors.push(v)
+			end
+
+			i += 1
+
+		end
+
+		#print the last number stored in the array
+		puts factors[-1]
+
+		#primes.each {|nums| puts nums}
+		#factors.each {|nums| puts nums}
+	
+	end
 end
 
-findFactor(13195)
+#factor = Primes.new(13195)
+factor = Primes.new(600851475143)
+factor.findFactors
